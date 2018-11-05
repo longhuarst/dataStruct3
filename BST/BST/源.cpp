f4195fbd0b413b9157c67ac95e095cb97fd60609
longhuarst <841105197@qq.com>
2018-11-05 15:37:57 +0800

    创建非二叉搜索树用于测试 二叉搜索树的插入功能 和 搜索功能

---
+++ BST/BST/源.cpp
@@ -0,0 +1,196 @@
+#include <iostream>
+
+#include <queue>
+
+using namespace std;
+
+
+
+
+
+
+typedef struct BinaryTreeNode{
+	int data;
+	struct BinaryTreeNode *left, *right;
+}BinaryTreeNode, *BinaryTree;
+
+
+
+
+
+
+
+
+
+
+//层次遍历创建树  注意 这棵树不是 二叉搜索树 只是为了测试  之后用二叉搜索树的插入来创建 二叉搜索树
+
+void CreateTree(BinaryTree &root, int size)
+{
+
+	if (root != NULL) {
+		cout << "树已存在！" << endl;
+	}
+	else {
+		
+		queue<BinaryTree> Q;
+
+		root = new BinaryTreeNode;
+		root->data = 1;
+		root->left = NULL;
+		root->right = NULL;
+
+		Q.push(root);
+		
+		int counter = 1;
+
+		if (size <= 1)
+			return;
+
+		while (!Q.empty()){
+		
+			BinaryTree p = Q.front();
+			Q.pop();
+
+			p->left = new BinaryTreeNode;
+			p->left->data = ++counter;
+			p->left->left = NULL;
+			p->left->right = NULL;
+
+			if (counter >= size)
+				return;
+
+			p->right = new BinaryTreeNode;
+			p->right->data = ++counter;
+			p->right->left = NULL;
+			p->right->right = NULL;
+
+			if (counter >= size)
+				return;
+
+			Q.push(p->left);
+			Q.push(p->right);
+		}
+
+	}
+	
+}
+
+
+void visit(BinaryTree root)
+{
+	cout << root->data << " " ;
+}
+
+
+
+
+
+
+//层次遍历
+void levelOrder(BinaryTree root)
+{
+
+	if (root == NULL)
+		return;
+	queue<BinaryTree> Q;
+	Q.push(root);
+
+	while (!Q.empty()) {
+		BinaryTree p = Q.front();
+		Q.pop();
+		visit(p);
+		if (p->left) Q.push(p->left);
+		if (p->right) Q.push(p->right);
+	}
+}
+
+
+
+BinaryTree root = NULL;
+
+
+
+
+BinaryTree Search(BinaryTree root,int data)
+{
+	if (!root) {
+		return NULL;
+	}
+
+	if (data < root->data) {
+		return Search(root->left, data);
+	}
+	else if (data > root->data) {
+		return Search(root->right, data);
+	}
+	else {
+		return root;
+	}
+}
+
+
+
+void Insert(BinaryTree &root, int data)
+{
+	if (!root) {
+		root = new BinaryTreeNode;
+		root->data = data;
+		root->left = NULL;
+		root->right = NULL;
+	}
+	else {
+		if (data < root->data) {
+			Insert(root->left, data);
+		}
+		else if (data > root->data) {
+			Insert(root->right, data);
+		}
+		else {
+			cout << "插入了重复的元素！" << endl;
+		}
+	}
+
+}
+
+int main() 
+{
+
+
+	cout << "层次遍历创建树  注意 这棵树不是 二叉搜索树 只是为了测试  之后用二叉搜索树的插入来创建 二叉搜索树" << endl;
+	CreateTree(root,25);
+
+
+
+	cout << "层次遍历" << endl;
+	levelOrder(root);
+
+
+	BinaryTree p = Search(root,15);
+
+	cout << "查找15结果= " << p->data;
+	
+
+
+	p = Search(root, 26);
+
+	if (p) {
+		cout << "查找26结果= " << p->data;
+	}else{
+		cout << "查找26结果= 未找到！" << endl;
+	}
+	
+
+
+	Insert(root, 26);
+
+	Insert(root, 26);
+
+	
+	levelOrder(root);
+
+
+
+
+	system("pause");
+}
